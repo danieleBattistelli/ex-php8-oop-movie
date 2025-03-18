75e4be7c2b38a7e0be7f7a28f292d4617488dd78
danieleBattistelli <bomber81@gmail.com>
2025-03-18 11:46:04 +0100

    feat: bonus2

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
 <?php
 
-class Genre {
+class Genre
+{
     public $name;
 
     // Costruttore
-    public function __construct($name) {
+    public function __construct($name)
+    {
         $this->name = $name;
     }
 }
 
-class Movie {
+trait Rating
+{
+    public $rating;
+
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+    }
+
+    public function getRating()
+    {
+        return $this->rating;
+    }
+}
+
+class Movie
+{
+    use Rating;
     public $title;
     public $director;
     public $year;
     public $genres;
 
     // Costruttore
-    public function __construct($title, $director, $year, $genres) {
+    public function __construct($title, $director, $year, $genres)
+    {
         $this->title = $title;
         $this->director = $director;
         $this->year = $year;
@@ -24,8 +44,9 @@ class Movie {
     }
 
     // Metodo per ottenere informazioni sul film
-    public function getInfo() {
-        $genres = array_map(function($genre) {
+    public function getInfo()
+    {
+        $genres = array_map(function ($genre) {
             return $genre->name;
         }, $this->genres);
         $genres_str = implode(", ", $genres);
@@ -41,9 +62,15 @@ $genre2 = new Genre("Action");
 $movie1 = new Movie("Inception", "Christopher Nolan", 2010, [$genre1, $genre2]);
 $movie2 = new Movie("The Matrix", "Lana Wachowski, Lilly Wachowski", 1999, [$genre2]);
 
+// Impostazione del rating per i film
+$movie1->setRating(8.8);
+$movie2->setRating(8.7);
+
 // Stampa delle proprietà dei film
 echo $movie1->getInfo();
 echo "<br>";
+echo "Rating: " . $movie1->getRating();
+echo "<br>";
 echo $movie2->getInfo();
-
-?>
+echo "<br>";
+echo "Rating: " . $movie2->getRating();
